74c87750c522087054ac02c8ea49d4e508560f49
Ankita Dey <138559097+hashtag-ankita@users.noreply.github.com>
2025-07-20 03:33:50 +0530

    Added entities.py and utils.py in scripts folder
    Created PhysicsEntity object for representing the player and its movements
    
    Created utility functions to load images

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_image
 
 class Game:
     def __init__(self):
@@ -7,16 +9,38 @@ class Game:
 
         pygame.display.set_caption('Ninja Game')
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
+        self.movement = [False, False]
+
+        self.assets = {
+            'player': load_image('entities/player.png')
+        }
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
     def run(self):
         while True:
+            self.display.fill((14, 219, 248))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
 
+            self.screen.blit(pygame.transform.scale(self.display, (640, 480)), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
