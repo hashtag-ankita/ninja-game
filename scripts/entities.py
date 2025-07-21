baf0b270427f5042ac57bce7a0b9e3b930f5d41b
Ankita Dey <138559097+hashtag-ankita@users.noreply.github.com>
2025-07-21 13:20:30 +0530

    Added the mechanism of collision detection and player jumping

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,11 +8,54 @@ class PhysicsEntity:
         self.size = size
         self.velocity = [0, 0]
 
-    def update(self, movement=(0, 0)):
+        self.collision = {
+            'left': False,
+            'right': False,
+            'up': False,
+            'down': False
+        }
+
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def update(self, tilemap, movement=(0, 0)):
+        self.collision = {
+            'left': False,
+            'right': False,
+            'up': False,
+            'down': False
+        }
+
         frame_movement = [movement[0] + self.velocity[0], movement[1] + self.velocity[1]]
 
         self.pos[0] += frame_movement[0]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0: # Moving right and collided with a tile                    
+                    entity_rect.right = rect.left
+                    self.collision['right'] = True
+                if frame_movement[0] < 0: # Moving left and collided with a tile
+                    entity_rect.left = rect.right
+                    self.collision['left'] = True
+                self.pos[0] = entity_rect.x
+
         self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0: # Moving down and collided with a tile
+                    entity_rect.bottom = rect.top
+                    self.collision['down'] = True
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                    self.collision['up'] = True
+                self.pos[1] = entity_rect.y
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1) # type: ignore
+
+        if self.collision['down'] or self.collision['up']:
+            self.velocity[1] = 0
 
     def render(self, surf):
         surf.blit(self.game.assets['player'], self.pos)
