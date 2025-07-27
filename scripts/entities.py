4e9a6dd8f2404fb38ec35e725fecb4c901683231
Ankita Dey <138559097+hashtag-ankita@users.noreply.github.com>
2025-07-27 18:08:48 +0530

    Added animation of player
    Animated the player's idle, run and jump states
    
    Created a new Player object inherited from PhysicsEntity

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,18 +8,28 @@ class PhysicsEntity:
         self.size = size
         self.velocity = [0, 0]
 
-        self.collision = {
+        self.collisions = {
             'left': False,
             'right': False,
             'up': False,
             'down': False
         }
 
+        self.action = ''
+        self.anim_offset = (-3, -3)
+        self.flip = False
+        self.set_action('idle')
+
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+    
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+            self.animation = self.game.assets[self.type + '/' + self.action].copy()
 
     def update(self, tilemap, movement=(0, 0)):
-        self.collision = {
+        self.collisions = {
             'left': False,
             'right': False,
             'up': False,
@@ -34,10 +44,10 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0: # Moving right and collided with a tile                    
                     entity_rect.right = rect.left
-                    self.collision['right'] = True
+                    self.collisions['right'] = True
                 if frame_movement[0] < 0: # Moving left and collided with a tile
                     entity_rect.left = rect.right
-                    self.collision['left'] = True
+                    self.collisions['left'] = True
                 self.pos[0] = entity_rect.x
 
         self.pos[1] += frame_movement[1]
@@ -46,16 +56,43 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0: # Moving down and collided with a tile
                     entity_rect.bottom = rect.top
-                    self.collision['down'] = True
+                    self.collisions['down'] = True
                 if frame_movement[1] < 0:
                     entity_rect.top = rect.bottom
-                    self.collision['up'] = True
+                    self.collisions['up'] = True
                 self.pos[1] = entity_rect.y
+
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0:
+            self.flip = True
 
         self.velocity[1] = min(5, self.velocity[1] + 0.1) # type: ignore
 
-        if self.collision['down'] or self.collision['up']:
+        if self.collisions['down'] or self.collisions['up']:
             self.velocity[1] = 0
 
+        self.animation.update()
+
     def render(self, surf, offset=(0, 0)):
-        surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
+        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
+
+
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'player', pos, size)
+        self.air_time = 0
+
+    def update(self, tilemap, movement=(0, 0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+        if self.collisions['down']:
+            self.air_time = 0
+
+        if self.air_time > 4:
+            self.set_action('jump')
+        elif movement[0] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
